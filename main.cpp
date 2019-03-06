51ccefa93c60ece1de49d893e3e6c1467c2fdb3a
MaduRajapaksha <rajapakshamadu6543@gmail.com>
2019-03-06 23:15:10 +0530

    Final Output

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@
 #include "sphere.h"
 #include "float.h"
 #include "camera.h"
+#include "material.h"
 
-float hit_sphere(const vec3& center, float radius, const ray& r) {
+/*float hit_sphere(const vec3& center, float radius, const ray& r) {
 	vec3 oc = r.origin() - center;
 	float a = dot(r.direction(), r.direction());
 	float b = 2.0* dot(oc, r.direction());
@@ -21,11 +22,15 @@ float hit_sphere(const vec3& center, float radius, const ray& r) {
 	}
 
 }
-
-vec3 color(const ray& r,hitable *world) {
+*/
+/*vec3 color(const ray& r,hitable *world) {
 	hit_record rec;
 	if (world->hit(r, 0.0, FLT_MAX, rec)) {
-		return 0.5*vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+		//return 0.5*vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5*color(ray(rec.p, target - rec.p), world);
+
+
 	}
 	else {
 		vec3 unit_direction = unit_vector(r.direction());
@@ -34,15 +39,112 @@ vec3 color(const ray& r,hitable *world) {
 	}
 	
 }
+*/
+vec3 color(const ray& r, hitable *world, int depth) {
+	hit_record rec;
+	if (world->hit(r, 0.001, FLT_MAX, rec)) {
+		ray scattered;
+		vec3 attenuation;
+		if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+			random_in_unit_sphere;            return attenuation * color(scattered, world, depth + 1);
+		}
+		else {
+			return vec3(0, 0, 0);
+		}
+	}
+	else {
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5*(unit_direction.y() + 1.0);
+		return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+	}
+}
+hitable *random_scene() {
+	int n = 500;
+	hitable **list = new hitable*[n + 1];
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+	int i = 1;
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			float choose_mat = rand();
+			vec3 center(a + 0.9*rand(), 0.2, b + 0.9*rand());
+			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+				if (choose_mat < 0.8) {  // diffuse
+					list[i++] = new sphere(center, 0.2, new lambertian(vec3(rand()*rand(), rand()*rand(), rand()*rand())));
+				}
+				else if (choose_mat < 0.95) { // metal
+					list[i++] = new sphere(center, 0.2,
+						new metal(vec3(0.5*(1 + rand()), 0.5*(1 + rand()), 0.5*(1 + rand())), 0.5*rand()));
+				}
+				else {  // glass
+					list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+				}
+			}
+		}
+	}
+
+	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+	return new hitable_list(list, i);
+}
 
 int main() {
 	std::ofstream  f;
 
-	f.open("new.ppm");
+	f.open("final.ppm");
 
-	int nx = 200;
-	int ny = 100;
-	int ns = 100;
+	int nx = 1200;
+	int ny = 800;
+	int ns = 10;
+	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	hitable *list[5];
+	float R = cos(M_PI / 4);
+	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
+	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.0));
+	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
+	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
+	hitable *world = new hitable_list(list, 5);
+	world = random_scene();
+
+	vec3 lookfrom(13, 2, 3);
+	vec3 lookat(0, 0, 0);
+	float dist_to_focus = 10.0;
+	float aperture = 0.1;
+
+	camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus);
+
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			vec3 col(0, 0, 0);
+			for (int s = 0; s < ns; s++) {
+				float u = float(i + rand()) / float(nx);
+				float v = float(j + rand()) / float(ny);
+				ray r = cam.get_ray(u, v);
+				vec3 p = r.point_at_parameter(2.0);
+				col += color(r, world, 0);
+			}
+			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+			int ir = int(255.99*col[0]);
+			int ig = int(255.99*col[1]);
+			int ib = int(255.99*col[2]);
+			std::cout << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+	f.close();
+}
+
+/*
+int main() {
+	std::ofstream  f;
+
+	f.open("diffuse.ppm");
+
+	int nx = 400;
+	int ny = 200;
+	int ns = 200;
 
 	f << "P3\n" << nx << " " << ny << "\n255\n";
 
@@ -51,7 +153,7 @@ int main() {
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0);
 	*/
-	hitable *list[2];
+	/*hitable *list[2];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5);
 	list[1] = new sphere(vec3(0, -100.5, -1), 100);
 	hitable *world = new hitable_list(list, 2);
@@ -90,7 +192,7 @@ int main() {
 			int ig = int(255.99*col[1]);
 			int ib = int(255.99*col[2]);
 			*/
-			vec3 col(0, 0, 0);
+			/*vec3 col(0, 0, 0);
 			for (int s = 0; s < ns; s++) {
 				float u = float(i + rand()) / float(nx);
 				float v = float(j + rand()) / float(ny);
@@ -105,6 +207,6 @@ int main() {
 			f << ir << " " << ig << " " << ib << "\n";
 		}
 	}
-
-	f.close();
-}
+	*/
+	//f.close();
+//} 
